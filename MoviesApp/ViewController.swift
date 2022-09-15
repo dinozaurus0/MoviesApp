ec7e80072a80ee0ed32381c39489029ead219180
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2022-09-15 15:02:56 +0300

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Vlad Grigore Sima on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
