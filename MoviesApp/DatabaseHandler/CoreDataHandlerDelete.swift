da4a2dd00969c6340c40d9d4e22da853f13b4e6f
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2022-12-09 15:49:54 +0200

    Removed the old and unused method to delete `NSManagedObject`. Solved the bug by calling the correct method in the `async` version of 'delete'.

--- MoviesApp/DatabaseHandler/CoreDataHandlerDelete.swift
+++ MoviesApp/DatabaseHandler/CoreDataHandlerDelete.swift
@@ -8,60 +8,12 @@
 import CoreData
 
 extension CoreDataHandler {
-    typealias DeleteResult = Swift.Result<Void, Error>
-
-    @available(*, deprecated, message: "Should use the async/await version")
-    internal func delete(objectsId: [NSManagedObjectID], in context: NSManagedObjectContext, completion: @escaping (DeleteResult) -> Void) {
-        context.perform { [weak self] in
-            guard let self = self else { return }
-
-            do {
-                let result = try self.executeDeleteIn(objectsId: objectsId, in: context)
-                let saveResult = self.executeSaveIfNeededIn(on: result, in: context)
-                completion(saveResult)
-            } catch(let error) {
-                completion(.failure(error))
-            }
-        }
-    }
-
-    private func executeSaveIfNeededIn(on result: DeleteResult, in context: NSManagedObjectContext) -> DeleteResult {
-        return result.flatMap {
-            guard self.shouldExecuteSave(on: context) else {
-                return .failure(DatabaseSaveError())
-            }
-
-            do {
-                try executeSave(on: context)
-                return .success(())
-            } catch(let error) {
-                return .failure(error)
-            }
-        }
-    }
-
-    private func executeDeleteIn(objectsId: [NSManagedObjectID], in context: NSManagedObjectContext) throws -> DeleteResult {
-        try objectsId.forEach { id in
-            do {
-                let object = try context.existingObject(with: id)
-                context.delete(object)
-            } catch {
-                context.rollback()
-                throw DatabaseDeleteError()
-            }
-        }
-
-        return .success(())
-    }
-}
-
-extension CoreDataHandler {
     internal func delete(objectsId: [NSManagedObjectID], in context: NSManagedObjectContext) async throws {
         return try await context.perform { [weak self] in
             guard let self = self else { throw DatabaseSaveError() }
 
             do {
-                try self.executeSave(on: context)
+                try self.executeDelete(objectsId: objectsId, in: context)
                 try self.executeSaveIfNeeded(in: context)
             } catch (let error) {
                 throw error
@@ -69,7 +21,7 @@ extension CoreDataHandler {
         }
     }
 
-    private func executeSaveIfNeeded(in context: NSManagedObjectContext) throws  {
+    private func executeSaveIfNeeded(in context: NSManagedObjectContext) throws {
         guard self.shouldExecuteSave(on: context) else {
             throw DatabaseSaveError()
         }
